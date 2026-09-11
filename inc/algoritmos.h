ad13b0f02f01433c71c3c2a664bd527c619e649e
Daniel Uribe <daniuribe13puq@gmail.com>
2026-09-10 23:17:56 -0300

    Añadir BusquedaBinaria

---
+++ inc/algoritmos.h
@@ -0,0 +1,13 @@
+#ifndef ALGORTIMO
+#define ALGORITMO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//aca definamos los algoritmos que usaremos 
+
+int binarySearch (int arr[], int low, int high, int x); //arreglo, 0, numero maximo del arreglo-1, numero a buscar
+
+#endif
---
+++ src/algortimos.c
@@ -0,0 +1,17 @@
+#include "algoritmos.h"
+
+int binarySearch (int arr[], int low, int high, int x)
+{
+    if (low > high)
+        return -1;
+    else 
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch (arr, low, mid - 1, x);
+        else
+            return binarySearch (arr, mid + 1,high, x);
+    }
+}
